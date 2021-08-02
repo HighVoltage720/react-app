52670f6f513a459b7a61e5476c310e6f16137dd0
Crayton Hudspeth <crayton.hudspeth@lexisnexisrisk.com>
2021-08-02 13:19:38 -0400

    prettier run

--- release.config.js
+++ release.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-    branches: "master",
-    repositoryUrl: "https://github.com/HighVoltage720/react-app",
-    plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', '@semantic-release/npm', '@semantic-release/github']
-}
+  branches: "master",
+  repositoryUrl: "https://github.com/HighVoltage720/react-app",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    "@semantic-release/github",
+  ],
+};
